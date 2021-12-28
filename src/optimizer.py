4e53e2c23af97673653b6d2696dfaedccadd0974
Kristjan Roosild <kristjan.roosild@wise.com>
2021-12-28 14:14:18 +0200

    refactor

--- src/optimizer.py
+++ src/optimizer.py
@@ -40,14 +40,13 @@ class HyperBoostOptimizer(object):
 
     def process(self, trials, algo):
         try:
-            result = fmin(fn=self.fn, space=self.space, algo=algo, trials=trials, early_stop_fn=self.early_stop_fn)
+            result = fmin(fn=self.crossvalidate, space=self.space, algo=algo, trials=trials, early_stop_fn=self.early_stop_fn)
         except Exception as e:
             return {'status': STATUS_FAIL,
                     'exception': str(e)}
         return result
 
-    # todo: remove duplicated crossvalidation code
-    def crossvalidate_xgboost(self, para):
+    def crossvalidate(self, para):
         folds = StratifiedKFold(n_splits=self.NFOLDS, shuffle=True, random_state=self.RANDOM_STATE)
         oof_preds = np.zeros((len(self.X), 1))
 
@@ -57,15 +56,7 @@ class HyperBoostOptimizer(object):
             trn_x, trn_y = sampler.fit_resample(self.X[train_index, :], self.y[train_index])
             val_x, val_y = self.X[valid_index, :], self.y[valid_index]
 
-            xg_train = xgb.DMatrix(trn_x, trn_y)
-            xg_valid = xgb.DMatrix(val_x, val_y)
-
-            clf = xgb.train(para['reg_params'],
-                            xg_train,
-                            evals=[(xg_train, "train"), (xg_valid, "eval")],
-                            **para['fit_params'])
-
-            val_pred = clf.predict(xgb.DMatrix(val_x))
+            val_pred = self.fn(para, trn_x, trn_y, val_x, val_y)
 
             print("AUC = {}".format(metrics.roc_auc_score(val_y, val_pred)))
             oof_preds[valid_index, :] = val_pred.reshape((-1, 1))
@@ -73,38 +64,31 @@ class HyperBoostOptimizer(object):
         loss = para['loss_func'](self.y, oof_preds.ravel())
         return {'loss': loss, 'status': STATUS_OK}
 
-    def crossvalidate_lightgbm(self, para):
-        folds = StratifiedKFold(n_splits=self.NFOLDS, shuffle=True, random_state=self.RANDOM_STATE)
-        oof_preds = np.zeros((len(self.X), 1))
+    def crossvalidate_xgboost(self, para, trn_x, trn_y, val_x, val_y):
+        trn_data = xgb.DMatrix(trn_x, trn_y)
+        val_data = xgb.DMatrix(val_x, val_y)
+        clf = xgb.train(para['reg_params'],
+                        trn_data,
+                        evals=[(trn_data, "train"), (val_data, "eval")],
+                        **para['fit_params'])
+        val_pred = clf.predict(xgb.DMatrix(val_x))
+        return val_pred
 
-        sampler = RandomOverSampler(random_state=self.RANDOM_STATE)
-
-        for fold_, (train_index, valid_index) in enumerate(folds.split(self.y, self.y)):
-            trn_x, trn_y = sampler.fit_resample(self.X[train_index, :], self.y[train_index])
-            val_x, val_y = self.X[valid_index, :], self.y[valid_index]
-
-            trn_data = lgb.Dataset(trn_x, trn_y, silent=True, params={'verbose': -1})
-            val_data = lgb.Dataset(val_x, val_y, silent=True, params={'verbose': -1})
-
-            num_round = 1000000
-            early_stopping_rounds = 3500
-
-            clf = lgb.train(para['reg_params'], trn_data, num_round,
-                            valid_sets=[trn_data, val_data],
-                            verbose_eval=-1,
-                            early_stopping_rounds=early_stopping_rounds)
-
-            val_pred = clf.predict(val_x, num_iteration=clf.best_iteration)
-
-            print("AUC = {}".format(metrics.roc_auc_score(val_y, val_pred)))
-            oof_preds[valid_index, :] = val_pred.reshape((-1, 1))
-
-        loss = para['loss_func'](self.y, oof_preds.ravel())
-        return {'loss': loss, 'status': STATUS_OK}
+    def crossvalidate_lightgbm(self, para, trn_x, trn_y, val_x, val_y):
+        trn_data = lgb.Dataset(trn_x, trn_y, silent=True, params={'verbose': -1})
+        val_data = lgb.Dataset(val_x, val_y, silent=True, params={'verbose': -1})
+        num_round = 1000000
+        early_stopping_rounds = 3500
+        clf = lgb.train(para['reg_params'], trn_data, num_round,
+                        valid_sets=[trn_data, val_data],
+                        verbose_eval=-1,
+                        early_stopping_rounds=early_stopping_rounds)
+        val_pred = clf.predict(val_x, num_iteration=clf.best_iteration)
+        return val_pred
 
     def find_baseline_loss(self):
         this_para = copy(self.space)
         this_para['reg_params'] = dict()
-        baseline_loss = self.fn(this_para)['loss']
+        baseline_loss = self.crossvalidate(this_para)['loss']
         print(f'Baseline loss: {baseline_loss}')
         return baseline_loss
