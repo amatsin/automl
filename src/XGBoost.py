Baseline calculation fixed in optimizer. XGBoost is now binary classifier and not regressor. Increased XGBoost early stopping patience.

src/XGBoost.py
@@ -21,25 +21,24 @@ def run():
     script_name = os.path.basename(__file__).split('.')[0]
     MODEL_NAME = "{0}__folds{1}".format(script_name, NFOLDS)
     print("Model: {}".format(MODEL_NAME))
-    train, test = load_data(remove_synth=False)
+    train = load_data()
     y = train.target.values
     train_ids = train.ID_code.values
     train = train.drop(['ID_code', 'target'], axis=1)
-    feature_list = train.columns
-    test_ids = test.ID_code.values
-    test = test[feature_list]
+
     X = train.values.astype(float)
-    X_test = test.values.astype(float)
     folds = StratifiedKFold(n_splits=NFOLDS, shuffle=True, random_state=RANDOM_STATE)
     sampler = RandomOverSampler(random_state=RANDOM_STATE)
 
     oof_preds = np.zeros((len(train), 1))
-    test_preds = np.zeros((len(test), 1))
     params = {
-        "eval_metric": "auc",
+        'objective': 'binary:logistic',
+        'eval_metric': 'auc'
     }
-    ITERATIONS = 1000
-    EARLY_STOP = 10
+
+    ITERATIONS = 4000
+    EARLY_STOP = 50
+
     config = dict(
         early_stop=EARLY_STOP,
         iterations=ITERATIONS,
@@ -64,24 +63,15 @@ def run():
         xg_valid = xgb.DMatrix(X_val, y_val)
 
         clf = xgb.train(params, xg_train, ITERATIONS, evals=[(xg_train, "train"), (xg_valid, "eval")],
-                        early_stopping_rounds=EARLY_STOP, verbose_eval=False, callbacks=[wandb_callback()])
+                        early_stopping_rounds=EARLY_STOP, verbose_eval=100, callbacks=[wandb_callback()])
 
         val_pred = clf.predict(xgb.DMatrix(X[valid_index, :]))
-        test_fold_pred = clf.predict(xgb.DMatrix(X_test))
 
         print("AUC = {}".format(metrics.roc_auc_score(y[valid_index], val_pred)))
         oof_preds[valid_index, :] = val_pred.reshape((-1, 1))
-        test_preds += test_fold_pred.reshape((-1, 1))
-    test_preds /= NFOLDS
+
     roc_score = metrics.roc_auc_score(y, oof_preds.ravel())
     print("Overall AUC = {}".format(roc_score))
-
-    print("Saving submission file")
-    sample = pd.read_csv('../input/santander-customer-transaction-prediction/sample_submission.csv')
-    sample.target = test_preds.astype(float)
-    sample.ID_code = test_ids
-    sample.to_csv('../model_predictions/submission_{}__{}.csv'.format(MODEL_NAME,
-                                                                      str(roc_score)), index=False)
 
     print("Saving OOF predictions")
     oof_preds = pd.DataFrame(np.column_stack((train_ids,

src/optimize_xgboost.py
@@ -1,9 +1,8 @@
 import argparse
 import warnings
 
-import numpy as np
 from hyperopt import hp
-from hyperopt import tpe, Trials
+from hyperopt import tpe
 from sklearn.metrics import roc_auc_score
 
 from optimizer import HyperBoostOptimizer
@@ -15,20 +14,24 @@ def parameters():
     # https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/
 
     xgb_reg_params = {
-        'eta':              hp.choice('eta', np.arange(0.05, 0.5, 0.05)),
-        'gamma':            hp.choice('gamma', np.concatenate((np.arange(0.05, 0.11, 0.01), [0.3, 0.5, 0.7, 0.9, 1.0]))),
-        'lambda':           hp.choice('alpha', [0.0, 0.1, 0.5, 1.0]),
-        'max_depth':        hp.choice('max_depth', [3, 5, 7, 9, 12, 15, 17, 25]),
-        'min_child_weight': hp.choice('min_child_weight', np.arange(1, 9, 2, dtype=int)),
-        'colsample_bytree': hp.choice('colsample_bytree', np.arange(0.3, 1.0, 0.1)),
+        'eta':              hp.uniform('eta', 0.05, 0.5),
+        'tree_method':      hp.choice('tree_method', ['exact', 'approx', 'hist']),
+        'gamma':            hp.choice('gamma', [hp.uniform('gamma_1', 0.05, 0.11), hp.uniform('gamma_2', 0.3, 1.0)]),
+        'lambda':           hp.uniform('alpha', 0.0, 1.0),
+        'max_depth':        hp.uniformint('max_depth', 2, 25),
+        'min_child_weight': hp.uniformint('min_child_weight', 1, 75),
+        'colsample_bytree': hp.uniform('colsample_bytree', 0.3, 1.0),
         'subsample':        hp.uniform('subsample', 0.6, 1),
-        'eval_metric': 'auc'
+        'eval_metric': 'auc',
+        'objective': 'binary:logistic'
     }
+
     xgb_fit_params = {
-        'num_boost_round': 1000,
-        'early_stopping_rounds': 10,
-        'verbose_eval': False
+        'num_boost_round': 4000,
+        'early_stopping_rounds': 50,
+        'verbose_eval': 100
     }
+
     xgb_para = dict()
     xgb_para['reg_params'] = xgb_reg_params
     xgb_para['fit_params'] = xgb_fit_params